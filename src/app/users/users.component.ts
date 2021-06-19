c6495c81925258afa6347f96002a425019283efa
lvijay29 <lvijay.tech@gmail.com>
2021-06-19 13:53:18 +0530

    Update users.component.ts

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FilterOption } from './filter-option.interface';
-
+import { UsersService } from '../users.service';
+import { HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-
+  public usersList;
+  selectedValue;
+  allUsersList;
   options: FilterOption[] = [
     {
       value: 'name',
@@ -31,9 +34,41 @@ export class UsersComponent implements OnInit {
     }
   ];
 
-  constructor() { }
+  constructor(public userService:UsersService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.selectedValue = 'name'
+    this.bindUserDetails()
+  }
+ async bindUserDetails() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json' ,
+        'Accept' : 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      })
+    };
+    let response = await this.getUsers(httpOptions)
+    this.usersList = response;
+    this.allUsersList = response;
+  }
+
+  changeOption(value)
+  {
+    this.selectedValue = value
+  }
+  filters(item) {
+    this.usersList = this.allUsersList;
+    this.usersList = this.usersList.filter( items => {
+      return items[this.selectedValue].toLowerCase().includes(item);
+    });
+  }
+  public getUsers(header) {
+    return new Promise((resolve, reject) => {
+      this.userService.getUserList(header).subscribe((data: any) => {
+        resolve(data);
+      });
+    });
   }
 
 }

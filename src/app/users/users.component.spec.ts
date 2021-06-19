d4029ae3754139951e0b70e281062dabad60fdd5
lvijay29 <lvijay.tech@gmail.com>
2021-06-19 13:54:04 +0530

    Update users.component.spec.ts

--- src/app/users/users.component.spec.ts
+++ src/app/users/users.component.spec.ts
@@ -18,8 +18,5 @@ describe('UsersComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  
 });

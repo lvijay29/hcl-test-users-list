cd37609955546ff3498864e3e4df6bd886f92c0a
lvijay29 <lvijay.tech@gmail.com>
2021-06-19 11:58:46 +0530

    Update users.service.ts

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  public getUserList(request: any, header): Observable<any> {
+    const url = this.getUrl('https://jsonplaceholder.typicode.com/users');
+    return this.http.get<any>(url, header);
+  }
 }
